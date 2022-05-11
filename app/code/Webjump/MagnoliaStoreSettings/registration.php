fa8b866398853616673c014428bfe75663d33d19
LuizFrareWJ <luiz.frare@webjump.com.br>
2022-05-11 14:55:50 -0300

    Add data patch to configure Magnolia by default

--- app/code/Webjump/MagnoliaStoreSettings/registration.php
+++ app/code/Webjump/MagnoliaStoreSettings/registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Webjump_MagnoliaSettings',
+    'Webjump_MagnoliaStoreSettings',
     __DIR__
 );
