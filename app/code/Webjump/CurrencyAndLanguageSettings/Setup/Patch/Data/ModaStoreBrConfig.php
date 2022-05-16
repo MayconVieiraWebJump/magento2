2da48f1385cc889214f298b309d07a1c9c0a42ea
JoaoVicenteRangelWJ <joao.rangel@webjump.com.br>
2022-05-16 14:17:48 -0300

    Added language and currency configurations

---
+++ app/code/Webjump/CurrencyAndLanguageSettings/Setup/Patch/Data/ModaStoreBrConfig.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Webjump\CurrencyAndLanguageSettings\Setup\Patch\Data;
+
+use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+
+class ModaStoreBrConfig implements DataPatchInterface
+{
+    
+    private $moduleDataSetup;
+    private $writer;
+    private $storeManager;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        WriterInterface $writer,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->writer = $writer;
+        $this->storeManager = $storeManager;
+    }
+
+    public function getAliases()
+    {
+        return [
+        ];
+    }
+
+    public static function getDependencies()
+    {
+        return [
+        ];
+    }
+    
+    public function apply()
+    {
+   
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        
+        $storeId = $this->storeManager
+            ->getStore("msvBR")
+            ->getId();
+
+        $this->writer->save(
+            "general/locale/code",
+            "pt_BR",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+            "general/locale/weight_unit",
+            "kgs",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+            "general/locale/timezone",
+            "America/Sao_Paulo",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+           "currency/options/allow",
+           "BRL",
+           "stores",
+           $storeId 
+        );
+
+        $this->writer->save(
+            "currency/options/default",
+            "BRL",
+            "stores",
+            $storeId
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+}
---
+++ app/code/Webjump/CurrencyAndLanguageSettings/Setup/Patch/Data/ModaStoreEnConfig.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Webjump\CurrencyAndLanguageSettings\Setup\Patch\Data;
+
+use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+
+class ModaStoreEnConfig implements DataPatchInterface
+{
+    
+    private $moduleDataSetup;
+    private $writer;
+    private $storeManager;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        WriterInterface $writer,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->writer = $writer;
+        $this->storeManager = $storeManager;
+    }
+
+    public function getAliases()
+    {
+        return [
+        ];
+    }
+
+    public static function getDependencies()
+    {
+        return [
+        ];
+    }
+    
+    public function apply()
+    {
+   
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $storeId = $this->storeManager
+            ->getStore("wsvEN")
+            ->getId();
+
+        $this->writer->save(
+            "general/locale/code",
+            "en_US",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+            "general/locale/weight_unit",
+            "lbs",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+            "general/locale/timezone",
+            "America/Los_Angeles",
+            "stores",
+            $storeId
+        );
+
+        $this->writer->save(
+           "currency/options/allow",
+           "USD",
+           "stores",
+           $storeId 
+        );
+
+        $this->writer->save(
+            "currency/options/default",
+            "USD",
+            "stores",
+            $storeId
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+}
