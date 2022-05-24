cc85e7a1af1206d52e156a16fc44be80e49ad352
LuizFrareWJ <luiz.frare@webjump.com.br>
2022-05-24 07:40:40 -0300

    FInalzed carousel

--- app/code/Webjump/MagnoliaSettings/Setup/Patch/Data/CreateCarouselBlock.php
+++ app/code/Webjump/MagnoliaSettings/Setup/Patch/Data/CreateCarouselBlock.php
@@ -12,25 +12,25 @@ use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\Patch\PatchRevertableInterface;
 use Magento\Store\Model\StoreManagerInterface;
 
-
-class CreateCarouselBlock implements DataPatchInterface, PatchRevertableInterface
+class createCarouselMagnoliaBlock implements DataPatchInterface, PatchRevertableInterface
 {
-  private StoreManagerInterface $storeManager;
-
-  private 
     /**
      * @var ModuleDataSetupInterface
      */
+    private $moduleDataSetup;
 
-    const IDENTIFIER_BLOCK = 'carrossel_moda_br';
-
-     /**
+    /**
      * @var BlockFactory
      */
     private $blockFactory;
 
+    const IDENTIFIER_BLOCK = 'carrosel_magnolia_br';
+
+    private StoreManagerInterface $storeManager;
+
+
     /**
-     * CreateCarouselBlock constructor.
+     * CreateBannerBlock constructor.
      * @param ModuleDataSetupInterface $moduleDataSetup
      * @param BlockFactory $blockFactory
      */
@@ -45,14 +45,14 @@ class CreateCarouselBlock implements DataPatchInterface, PatchRevertableInterfac
     }
 
     /**
-     * Patch Method "Apply" method $this->createCarouselBlock()
+     * Patch Method "Apply" method $this->createBannerBlock()
      * @return DataPatchInterface|void
      */
     public function apply()
     {
         $this->moduleDataSetup->getConnection()->startSetup();
 
-        $this->createCarouselBlock("moda_br");
+        $this->createCarouselMagnoliaBlock("msvBR");
 
         $this->moduleDataSetup->getConnection()->endSetup();
     }
@@ -60,18 +60,18 @@ class CreateCarouselBlock implements DataPatchInterface, PatchRevertableInterfac
     /**
      *  Method to create cms landing page
      */
-    protected function createCarouselBlock(string $websiteCode)
+    protected function createCarouselMagnoliaBlock(string $storeCode)
     {
-      $websiteGetId = $this->storeManager->getWebsite($websiteCode)->getId();
+        $storeGetId = $this->storeManager->getStore($storeCode)->getId();
 
-      $cmsBlock = $this->blockFactory->create()->load(self::IDENTIFIER_BLOCK, 'identifier');
+        $cmsBlock = $this->blockFactory->create()->load(self::IDENTIFIER_BLOCK, 'identifier');
 
         $cmsBlockData = [
             'is_active' => 1,
-            'title' => 'Carrossel Moda pt-br',
+            'title' => 'Carroussel Magnolia pt-br',
             'identifier' => self::IDENTIFIER_BLOCK,
-            'stores' => [$websiteGetId],
-            'content' => '<style>#html-body [data-pb-style=FORS53K],#html-body [data-pb-style=NMQX3L7]{justify-content:flex-start;display:flex;flex-direction:column;background-repeat:no-repeat;background-attachment:scroll;text-align:center}#html-body [data-pb-style=FORS53K]{background-position:center center;background-size:contain;min-height:58px}#html-body [data-pb-style=NMQX3L7]{background-position:left top;background-size:cover}#html-body [data-pb-style=G8VTL12]{text-align:center}</style><div data-content-type="row" data-appearance="contained" data-element="main"><div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{\&quot;desktop_image\&quot;:\&quot;{{media url=wysiwyg/3-layers.png}}\&quot;,\&quot;mobile_image\&quot;:\&quot;{{media url=wysiwyg/novidades_para_voc_1_.png}}\&quot;}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="inner" data-pb-style="FORS53K"></div></div><div data-content-type="row" data-appearance="full-width" data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="main" data-pb-style="NMQX3L7"><div class="row-full-width-inner" data-element="inner"><div data-content-type="products" data-appearance="carousel" data-autoplay="false" data-autoplay-speed="4000" data-infinite-loop="false" data-show-arrows="true" data-show-dots="false" data-carousel-mode="default" data-center-padding="90px" data-element="main" data-pb-style="G8VTL12">{{widget type="Magento\CatalogWidget\Block\Product\ProductsList" template="Magento_PageBuilder::catalog/product/widget/content/carousel.phtml" anchor_text="" id_path="" show_pager="0" products_count="6" condition_option="category_ids" condition_option_value="6" type_name="Catalog Products Carousel" conditions_encoded="^[`1`:^[`aggregator`:`all`,`new_child`:``,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Combine`,`value`:`1`^],`1--1`:^[`operator`:`==`,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Product`,`attribute`:`category_ids`,`value`:`6`^]^]" sort_order="position"}}</div></div></div>'
+            'stores' => [$storeGetId],
+            'content' => '<style>#html-body [data-pb-style=KNV2HW0],#html-body [data-pb-style=LT9F265]{justify-content:flex-start;display:flex;flex-direction:column;background-position:center center;background-size:cover;background-repeat:no-repeat;background-attachment:scroll;text-align:center}#html-body [data-pb-style=LT9F265]{background-position:left top}#html-body [data-pb-style=M06TF2S]{padding-left:53px;padding-right:43px}#html-body [data-pb-style=MMQEBQ0]{margin-top:30px;margin-bottom:30px;border-style:none}#html-body [data-pb-style=VL5S8MT],#html-body [data-pb-style=W66NNBJ]{max-width:100%;height:auto}@media only screen and (max-width: 768px) { #html-body [data-pb-style=MMQEBQ0]{border-style:none} }</style><div data-content-type="row" data-appearance="full-width" data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="main" data-pb-style="LT9F265"><div class="row-full-width-inner" data-element="inner"><figure data-content-type="image" data-appearance="full-width" data-element="main" data-pb-style="MMQEBQ0"><img class="pagebuilder-mobile-hidden" src="{{media url=wysiwyg/3-layers_1_.png}}" alt="" title="" data-element="desktop_image" data-pb-style="W66NNBJ"><img class="pagebuilder-mobile-only" src="{{media url=wysiwyg/2-layers_1_.png}}" alt="" title="" data-element="mobile_image" data-pb-style="VL5S8MT"></figure></div></div><div data-content-type="row" data-appearance="contained" data-element="main"><div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="inner" data-pb-style="KNV2HW0"><div data-content-type="products" data-appearance="carousel" data-autoplay="false" data-autoplay-speed="4000" data-infinite-loop="false" data-show-arrows="true" data-show-dots="false" data-carousel-mode="default" data-center-padding="90px" data-element="main" data-pb-style="M06TF2S">{{widget type="Magento\CatalogWidget\Block\Product\ProductsList" template="Magento_PageBuilder::catalog/product/widget/content/carousel.phtml" anchor_text="" id_path="" show_pager="0" products_count="25" condition_option="category_ids" condition_option_value="6" type_name="Catalog Products Carousel" conditions_encoded="^[`1`:^[`aggregator`:`all`,`new_child`:``,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Combine`,`value`:`1`^],`1--1`:^[`operator`:`==`,`type`:`Magento||CatalogWidget||Model||Rule||Condition||Product`,`attribute`:`category_ids`,`value`:`6`^]^]" sort_order="position"}}</div></div></div>'
         ];
 
         if (!$cmsBlock->getId()) {
