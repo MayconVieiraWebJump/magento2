80adb2b9987c3243a794b37513e1b3ece85c1881
Maycon Vieira <maycon.mendes@webjump.com>
2022-05-26 11:47:48 -0300

    Add datapath and footer wine

---
+++ app/code/Webjump/WineClubSettings/Setup/Patch/Data/CreateFooterImageBlock.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Webjump\WineClubSettings\Setup\Patch\Data;
+
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class CreateFooterImageBlock implements DataPatchInterface, PatchRevertableInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+
+    const IDENTIFIER_BLOCK = 'footer_img';
+
+    private StoreManagerInterface $storeManager;
+
+
+    /**
+     * CreateBannerBlock constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param BlockFactory $blockFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockFactory $blockFactory,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Patch Method "Apply" method $this->createBannerBlock()
+     * @return DataPatchInterface|void
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $this->createFooterImageWineBlock("wsvBR");
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     *  Method to create cms landing page
+     */
+    protected function createFooterImageWineBlock(string $storeCode)
+    {
+        $storeGetId = $this->storeManager->getStore($storeCode)->getId();
+
+        $cmsBlock = $this->blockFactory->create()->load(self::IDENTIFIER_BLOCK, 'identifier');
+
+        $cmsBlockData = [
+            'is_active' => 1,
+            'title' => 'Footer Image Wine br',
+            'identifier' => self::IDENTIFIER_BLOCK,
+            'stores' => [$storeGetId],
+            'content' => '<style>#html-body [data-pb-style=EELJJOP]{justify-content:flex-start;display:flex;flex-direction:column;background-position:center center;background-size:cover;background-repeat:no-repeat;background-attachment:scroll}#html-body [data-pb-style=CKJOB41]{text-align:center;border-style:none}#html-body [data-pb-style=J0JSYK7],#html-body [data-pb-style=VYKQJ17]{max-width:100%;height:auto}@media only screen and (max-width: 768px) { #html-body [data-pb-style=CKJOB41]{border-style:none} }</style><div data-content-type="row" data-appearance="contained" data-element="main"><div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-background-type="image" data-video-loop="true" data-video-play-only-visible="true" data-video-lazy-load="true" data-video-fallback-src="" data-element="inner" data-pb-style="EELJJOP"><figure class="img-footer-wine" data-content-type="image" data-appearance="full-width" data-element="main" data-pb-style="CKJOB41"><img class="pagebuilder-mobile-hidden" src="{{media url=wysiwyg/Grupo_3_5.png}}" alt="" title="" data-element="desktop_image" data-pb-style="VYKQJ17"><img class="pagebuilder-mobile-only" src="{{media url=wysiwyg/Grupo_de_m_scara_10_5.png}}" alt="" title="" data-element="mobile_image" data-pb-style="J0JSYK7"></figure></div></div>'
+        ];
+
+        if (!$cmsBlock->getId()) {
+            $this->blockFactory->create()->setData($cmsBlockData)->save();
+        } else {
+            $cmsBlock->setTitle($cmsBlockData['title']);
+            $cmsBlock->setContent($cmsBlockData['content']);
+            $cmsBlock->save();
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
