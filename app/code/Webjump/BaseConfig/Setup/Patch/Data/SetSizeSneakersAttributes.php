c0b7821316a1d856b9aa244f62410bae243ba95a
Felipe N. Oliveira <felipe.oliveira@webjump.com.br>
2022-06-03 15:40:59 -0300

    Configuração de tamanho para produtos da loja Moda

--- app/code/Webjump/BaseConfig/Setup/Patch/Data/SetSizeSneakersAttributes.php
+++ app/code/Webjump/BaseConfig/Setup/Patch/Data/SetSizeSneakersAttributes.php
@@ -6,7 +6,7 @@ use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
-class SetTamanhoAttribute implements DataPatchInterface
+class SetSizeSneakersAttributes implements DataPatchInterface
 {
     private ModuleDataSetupInterface $moduleDataSetup;
     private EavSetupFactory $eavSetupFactory;
@@ -36,7 +36,7 @@ class SetTamanhoAttribute implements DataPatchInterface
 
         $eavSetup->addAttribute(
             \Magento\Catalog\Model\Product::ENTITY,
-            'tamanhoclo',
+            'tamanhosnek',
             [
                 'type' => 'int',
                 'backend' => '',
@@ -60,10 +60,22 @@ class SetTamanhoAttribute implements DataPatchInterface
                 'visible_in_advanced_search' => '1',
                 'option' => [
                     'values' => [
-                        'P',
-                        'M',
-                        'G',
-                        'GG'
+                        '30',
+                        '31',
+                        '32',
+                        '33',
+                        '34',
+                        '35',
+                        '36',
+                        '37',
+                        '38',
+                        '39',
+                        '40',
+                        '41',
+                        '42',
+                        '43',
+                        '44',
+                        '45'
                     ]
                 ]
             ]
@@ -81,7 +93,7 @@ class SetTamanhoAttribute implements DataPatchInterface
 
         $attributeId = $eavSetup->getAttributeId(
             $entityTypeId,
-            'tamanhoclo'
+            'tamanhosnek'
         );
 
         $eavSetup->addAttributeToSet(
